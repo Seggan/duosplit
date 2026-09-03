e69cb28f9ea9bcef638b9cc20c5fee9be4f46559
Seggan <seggan21@gmail.com>
2026-09-03 14:27:29 -0400

    Don't normalize by default

--- src/cli.rs
+++ src/cli.rs
@@ -49,6 +49,6 @@ pub struct Cli {
     #[arg(short, long, action, help = "Enable timing output")]
     pub timings: bool,
 
-    #[arg(short, long, action, default_value_t = true, help = "Normalize the images to each other")]
+    #[arg(short, long, action, default_value_t = false, help = "Normalize the images to each other")]
     pub normalize: bool,
 }
