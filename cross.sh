85d2d6d9b0fa2ef26731e84ff9195755fb84ce41
Seggan <seggan21@gmail.com>
2026-09-03 14:21:36 -0400

    Woops

--- cross.sh
+++ cross.sh
@@ -1,3 +1,6 @@
 cross build --release --target x86_64-unknown-linux-gnu
+cp target/x86_64-unknown-linux-gnu/release/duosplit target/x86_64-unknown-linux-gnu/release/duosplit-x86_64-unknown-linux-gnu
 cross build --release --target aarch64-unknown-linux-gnu
+cp target/aarch64-unknown-linux-gnu/release/duosplit target/aarch64-unknown-linux-gnu/release/duosplit-aarch64-unknown-linux-gnu
 cross build --release --target x86_64-pc-windows-gnu
+cp target/x86_64-pc-windows-gnu/release/duosplit target/x86_64-pc-windows-gnu/release/duosplit-x86_64-pc-windows-gnu
