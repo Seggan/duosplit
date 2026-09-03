dc26fc9dedb0d6670f1af7a502c8ee78e3183d28
Seggan <seggan21@gmail.com>
2026-09-03 14:22:44 -0400

    Again

--- cross.sh
+++ cross.sh
@@ -3,4 +3,4 @@ cp target/x86_64-unknown-linux-gnu/release/duosplit target/x86_64-unknown-linux-
 cross build --release --target aarch64-unknown-linux-gnu
 cp target/aarch64-unknown-linux-gnu/release/duosplit target/aarch64-unknown-linux-gnu/release/duosplit-aarch64-unknown-linux-gnu
 cross build --release --target x86_64-pc-windows-gnu
-cp target/x86_64-pc-windows-gnu/release/duosplit target/x86_64-pc-windows-gnu/release/duosplit-x86_64-pc-windows-gnu
+cp target/x86_64-pc-windows-gnu/release/duosplit.exe target/x86_64-pc-windows-gnu/release/duosplit-x86_64-pc-windows-gnu.exe
